1cd85b8ef8ded657f24b79db90cd8ed1e9717bd6
Viktor Wilhelm <viktor.wilhelm@freenet.de>
2024-03-19 23:10:53 +0100

    Style

--- js/kontaktformular.js
+++ js/kontaktformular.js
@@ -1,3 +1,24 @@
 function initKontaktformular() {
   includeHTML();
 }
+
+/*contact form*/
+
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  fetch("https://formspree.io/f/mrgwroqj", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(() => {
+      window.location.href = "./send_mail.html";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
